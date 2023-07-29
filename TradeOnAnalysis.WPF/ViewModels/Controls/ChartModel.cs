4a5f0524900ed676322541b43fa0cff062e9e19b
SmeDaSe <wateiwate2@mail.ru>
2023-07-29 09:29:43 +0300

    design changes

--- TradeOnAnalysis.WPF/ViewModels/Controls/ChartModel.cs
+++ TradeOnAnalysis.WPF/ViewModels/Controls/ChartModel.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Collections.ObjectModel;
 using System.Data;
 using System.Linq;
+using System.Windows.Controls;
 
 using LiveChartsCore;
 using LiveChartsCore.Defaults;
