32548cb4dfd1f6531e4458cace46da34d0543859
SmeDaSe <wateiwate2@mail.ru>
2023-09-15 20:43:26 +0300

    mini code change

--- TradeAnalysis.WPF/ViewModels/ViewModelBase.cs
+++ TradeAnalysis.WPF/ViewModels/ViewModelBase.cs
@@ -1,24 +1,23 @@
 ﻿using System.ComponentModel;
 using System.Runtime.CompilerServices;
 
-namespace TradeAnalysis.WPF.ViewModels
+namespace TradeAnalysis.WPF.ViewModels;
+
+public class ViewModelBase : INotifyPropertyChanged
 {
-    public class ViewModelBase : INotifyPropertyChanged
+    public event PropertyChangedEventHandler? PropertyChanged;
+
+    protected bool ChangeProperty<T>(ref T property, T value, [CallerMemberName] string? propertyName = null)
     {
-        public event PropertyChangedEventHandler? PropertyChanged;
+        if (property?.Equals(value) == true)
+            return false;
+        property = value;
+        PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        return true;
+    }
 
-        protected bool ChangeProperty<T>(ref T property, T value, [CallerMemberName] string? propertyName = null)
-        {
-            if (property?.Equals(value) == true)
-                return false;
-            property = value;
-            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
-            return true;
-        }
-
-        protected void OnChanged([CallerMemberName] string? propertyName = null)
-        {
-            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
-        }
+    protected void OnChanged([CallerMemberName] string? propertyName = null)
+    {
+        PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
     }
 }
